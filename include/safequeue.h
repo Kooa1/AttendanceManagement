7704c626ff84306a6c0f676dfe049f46702064da
shanhai <862190424@qq.com>
2025-11-25 16:40:08 +0800

    add safe queue

--- include/safequeue.h
+++ include/safequeue.h
@@ -25,38 +25,26 @@ private:
     std::atomic_bool stop_request;
     std::mutex q_mutex;
     const int max_size;
-    const int size_warning_line;
 
 public:
     explicit SafeQueue(const int max_size)
         : pause_request(false),
           stop_request(false),
-          max_size(max_size),
-          size_warning_line(static_cast<int>(max_size * 0.8)) {
+          max_size(max_size) {
     }
 
     bool push(T &&value) {
         std::unique_lock<std::mutex> unique_lock(q_mutex);
-        if (stop_request.load(std::memory_order_acquire)) {
-            return false;
-        }
-
-        if (pause_request.load(std::memory_order_acquire)) {
-            return false;
-        }
-
-        if (data_queue.size() > max_size) {
+        if (data_queue.size() >= max_size) {
             cv_full.wait(unique_lock, [this]() {
-                return stop_request.load(std::memory_order_acquire) |
-                       pause_request.load(std::memory_order_acquire) |
+                return stop_request.load(std::memory_order_acquire) ||
+                       pause_request.load(std::memory_order_acquire) ||
                        data_queue.size() < max_size;
             });
 
-            if (stop_request.load(std::memory_order_acquire)) {
+            if (stop_request.load(std::memory_order_acquire) || pause_request.load(std::memory_order_acquire)) {
                 return false;
             }
-        } else if (data_queue.size() > size_warning_line && data_queue.size() < max_size) {
-            std::this_thread::sleep_for(std::chrono::microseconds(1000));
         }
 
         data_queue.push(std::move(value));
@@ -66,21 +54,14 @@ public:
 
     std::optional<T> pop() {
         std::unique_lock<std::mutex> unique_lock(q_mutex);
-        if (stop_request.load(std::memory_order_acquire)) {
-            return std::nullopt;
-        }
-
-        if (pause_request.load(std::memory_order_acquire)) {
-            return std::nullopt;
-        }
         if (data_queue.empty()) {
             cv_empty.wait(unique_lock, [this] {
-                return stop_request.load(std::memory_order_acquire) |
-                       pause_request.load(std::memory_order_acquire) |
+                return stop_request.load(std::memory_order_acquire) ||
+                       pause_request.load(std::memory_order_acquire) ||
                        !data_queue.empty();
             });
 
-            if (stop_request.load(std::memory_order_acquire)) {
+            if (stop_request.load(std::memory_order_acquire) || pause_request.load(std::memory_order_acquire)) {
                 return std::nullopt;
             }
         }
@@ -92,10 +73,12 @@ public:
     }
 
     size_t size() {
+        std::unique_lock<std::mutex> unique_lock(q_mutex);
         return data_queue.size();
     }
 
     bool empty() {
+        std::unique_lock<std::mutex> unique_lock(q_mutex);
         return data_queue.empty();
     }
 };
